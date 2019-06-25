d56ff0ba98a1c429e70836d86285df86e2b76d11
SimonDaNinja <simon.liljebeach@gmail.com>
2019-06-25 14:35:36 +0200

    Rudimentary stage selection added

--- example_stage.py
+++ example_stage.py
@@ -8,8 +8,8 @@ from stage import Stage
 WIDTH = 800
 HEIGHT = 600
 
-STAGE_WIDTH = 1200
-STAGE_HEIGHT = 1000
+STAGE_WIDTH = WIDTH
+STAGE_HEIGHT = HEIGHT
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -17,7 +17,8 @@ character = Character(screen, int(WIDTH/2), int(HEIGHT/2), (255,215,0), 50)
 solids = []
 solids.append(Solid((int(255/2),0,0),[(0,200,math.floor(WIDTH/3),200),(math.floor(WIDTH/3),100,math.floor(WIDTH/3),100),(math.floor(WIDTH/3)*2,200,(WIDTH-1-math.floor(WIDTH/3)*2),200)]))
 solids.append(Solid((0,int(255/3*2),0),[(0,int(HEIGHT*2/3),250,50)]))
-stage = Stage(screen, character, False, STAGE_WIDTH, STAGE_HEIGHT, solids)
+isPeriodic = True
+stage = Stage(screen, character, isPeriodic, STAGE_WIDTH, STAGE_HEIGHT, solids)
 if __name__ == '__main__':
     stage.DrawStage(800)
     input('press anything to exit')
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 if __name__ == '__main__':
-    import new_stage as stage
+    choice = ''
+    while True:
+        choice = input('do you want the small stage [1] or the big stage [2]? ')
+        if (choice == '1') or (choice == '2'):
+            break
+        else:
+            print('Invalid response!')
+    if choice == '1':
+        import example_stage as stage
+    elif choice == '2':
+        import new_stage as stage
+    else:
+        print('Something went wrong!')
+        exit()
     stage.stage.Run()
